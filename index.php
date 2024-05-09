02d3f4c3ea315af72f298e3f15ffb91667bbe57f
karuku-design <annkaruku5832@gmail.com>
2024-05-09 16:13:43 +0300

    Changed index.php to home_page.php.

--- index.php
+++ index.php
@@ -3,107 +3,72 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Task Tracker</title>
+    <title>Employee Task Tracker</title>
+    
     <link rel="stylesheet" href="assets/css/all.min.css">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/fontawesome.min.css">
-    <link rel="stylesheet" href="assets/icons/feather/css/feather.css">
-    <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/jquery.dataTables.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 
-    <script src="assets/js/jquery.min.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/jquery.min.js"></script>
     <script src="assets/js/jquery.dataTables.js"></script>
 </head>
 <body>
-    <div class="preloader"></div>
-    
-    <header class="header">
-        <div class="title-tab">
-            <a href="index.php" class="navbar-brand">
-                <div class="icon">
-                    <img src="assets/images/arms.png" alt="">
-                </div>
-                <div class="title-text">ICT Task Tracker</div>
-            </a>
-            <!--<div class="screen">
-                <span class="feather icon-maximize scre"></span>
-            </div>-->
-        </div>
-        <div class="profile-tab">
-            <div class="profile-photo">
-                <img src="assets/images/pic-1.png" alt="" class="image-responsive">
-            </div>
-            <div class="profile-description">
-                <span>omar mathias</span>
-                <a href="#"><span class="feather icon-power text-danger"></span></a>
-            </div>
-        </div>
-    </header>
 
-    <aside class="sidebar">
-        <div class="sidebar-header">
-            <span class="text-fade">navigation</span>
-        </div>
-        <div class="sidebar-menu">
-            <a href="index.php" class="link"><span class="feather icon-home"></span><span>Dashboard</span></a>
-            <div class="drop">
-                <span>
-                    <span class="feather icon-clipboard"></span>
-                    <span>Task</span>
-                </span>
-                <span class="feather icon-chevron-right"></span>
+    <div class="preloader"></div>
+
+    <main class="m-footer">
+        <div class="container">
+            <div class="form-header text-center pt-4">
+                <img src="assets/images/arms.png" alt="">
+                <h4>REPUBLIC OF KENYA</h4>
+                <br><br>
+                <h4>MINISTRY OF INVESTMENTS, TRADE & INDUSTRY</h4>
+                <h4>STATE DEPARTMENT FOR TRADE</h4>
+                <h6 class="mt-5">ICT DEPARTMENT TECHNICAL SUPPORT REGISTER</h6>
             </div>
-            <div class="drop-content">
-                <a href="add_task.php" class="link"><span class="feather icon-chevron-right"></span><span>New Task</span></a>
-                <a href="tasks.php" class="link"><span class="feather icon-chevron-right"></span><span>View Task</span></a>
-            </div>
-            <div class="drop">
-                <span>
-                    <span class="feather icon-users"></span>
-                    <span>Officers</span>
-                </span>
-                <span class="feather icon-chevron-right"></span>
-            </div>
-            <div class="drop-content">
-                <a href="add_officer.php" class="link"><span class="feather icon-chevron-right"></span><span>New Officer</span></a>
-                <a href="officers.php" class="link"><span class="feather icon-chevron-right"></span><span>View Officer</span></a>
-            </div>
-            <a href="#" class="link"><span class="feather icon-user"></span><span>Account Settings</span></a>
-        </div>
-    </aside>
-    <main class="content">
-        <div class="content-header">
-            <div class="title">
-                <h4>Dashboard</h4>
-            </div>
-            <div class="navigation">
-                <span><a href="index.php"><i class="feather icon-home"></i></a></span>
-                <span>/</span>
-                <span class="text-fade">Dashboard</span>
-            </div>
-        </div>
-        <div class="content-body">
-            <div class="banner">
-                <div class="banner-text">
-                    <h1>MINISTRY OF INVESTMENTS, TRADE & INDUSTRY</h1>
-                    <h2>STATE DEPARTMENT FOR TRADE</h2>
-                    <h4>ICT DEPARTMENT TECHNICAL SUPPORT REGISTER</h4>
+            <div class="form-body mt-2">
+                <div class="row d-flex align-items-center justify-content-center">
+                    <div class="col-md-4">
+                        <div class="card">
+                            <div class="card-header">Login </div>
+                            <div class="card-body">
+                                <form action="home_page.php">
+                                    <div class="form-group mb-3">
+                                        <label for="" class="form-control-label">Select Type of User</label>
+                                        <select name="user_type" id="" class="form-control">
+                                            <option value="">----select user type---</option>
+                                            <option value="1">Admin</option>
+                                            <option value="2">Officer</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group mb-2">
+                                        <label for="" class="form-control-label" name="officer_code">Officer Code</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                    <div class="form-group mb-2">
+                                        <label for="" class="form-control-label" name="password">Password</label>
+                                        <input type="password" class="form-control">
+                                    </div>
+                                    <a href="" class="mb-2">forgot password</a>
+                                    <input type="submit" value="Login" class="btn btn-primary w-100">
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </main>
-    <footer>
-        <marquee behavior="alternate" direction="">
-            &copy; 2023 All Right Reserved <span>Developed By Omar, James, Sharon, Anthony, Faith and Cynthia</span>
-        </marquee>
-    </footer>
-    <script src="assets/js/custom.js"></script>
+
     <script>
-        $(document).ready(()=>{})
-        $('.preloader').fadeOut('slow', function(){
-            $(this).remove()
-        }).delay(100)
+        $(document).ready(()=>{
+            $('.preloader').fadeOut('slow', function(){
+                $(this).remove()
+            }).delay(100)
+        })
     </script>
 </body>
 </html>
