10c744e965e5c488dcaede4550496363111b76d0
karuku-design <annkaruku5832@gmail.com>
2024-05-09 15:01:09 +0300

    Added add_officer.php page

---
+++ add_officer.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task Tracker</title>
+    <link rel="stylesheet" href="assets/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+    <link rel="stylesheet" href="assets/icons/feather/css/feather.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/jquery.dataTables.css">
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/jquery.dataTables.js"></script>
+</head>
+<body>
+    <div class="preloader"></div>
+
+    <header class="header">
+        <div class="title-tab">
+            <a href="index.php" class="navbar-brand">
+                <div class="icon">
+                    <img src="assets/images/arms.png" alt="">
+                </div>
+                <div class="title-text">ICT Task Tracker</div>
+            </a>
+            <!--<div class="screen">
+                <span class="feather icon-maximize scre"></span>
+            </div>-->
+        </div>
+        <div class="profile-tab">
+            <div class="profile-photo">
+                <img src="assets/images/pic-1.png" alt="" class="image-responsive">
+            </div>
+            <div class="profile-description">
+                <span>omar mathias</span>
+                <a href="#"><span class="feather icon-power text-danger"></span></a>
+            </div>
+        </div>
+    </header>
+
+    <aside class="sidebar">
+        <div class="sidebar-header">
+            <span class="text-fade">navigation</span>
+        </div>
+        <div class="sidebar-menu">
+            <a href="index.php" class="link"><span class="feather icon-home"></span><span>Dashboard</span></a>
+            <div class="drop">
+                <span>
+                    <span class="feather icon-clipboard"></span>
+                    <span>Task</span>
+                </span>
+                <span class="feather icon-chevron-right"></span>
+            </div>
+            <div class="drop-content">
+                <a href="add_task.php" class="link"><span class="feather icon-chevron-right"></span><span>New Task</span></a>
+                <a href="tasks.php" class="link"><span class="feather icon-chevron-right"></span><span>View Task</span></a>
+            </div>
+            <div class="drop">
+                <span>
+                    <span class="feather icon-users"></span>
+                    <span>Officers</span>
+                </span>
+                <span class="feather icon-chevron-right"></span>
+            </div>
+            <div class="drop-content">
+                <a href="add_officer.php" class="link"><span class="feather icon-chevron-right"></span><span>New Officer</span></a>
+                <a href="officers.php" class="link"><span class="feather icon-chevron-right"></span><span>View Officer</span></a>
+            </div>
+            <a href="#" class="link"><span class="feather icon-user"></span><span>Account Settings</span></a>
+        </div>
+    </aside>
+    <main class="content">
+        <div class="content-header">
+            <div class="title">
+                <h4>Officer</h4>
+            </div>
+            <div class="navigation">
+                <span><a href="index.php"><i class="feather icon-home"></i></a></span>
+                <span>/</span>
+                <span class="text-fade">Add Officer</span>
+            </div>
+        </div>
+        <div class="content-body">
+            <form action="">
+                <div class="row">
+                    <div class="col-md-4 mb-3">
+                        <div class="form-group">
+                            <label for="" class="form-control-label">Officer Name</label>
+                            <input type="text" class="form-control" required>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="form-group">
+                            <label for="" class="form-control-label">Officer Designation</label>
+                            <input type="text" class="form-control" required>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="form-group">
+                            <label for="" class="form-control-label">Department</label>
+                            <input type="text" class="form-control" required>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="form-group">
+                            <label for="" class="form-control-label">Contact</label>
+                            <input type="number"  class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="form-group">
+                            <label for="" class="form-control-label">Officer Code</label>
+                            <input type="number" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="form-group">
+                            <label for="" class="form-control-label">Remarks</label>
+                            <textarea name="" id="" cols="30" rows="1" class="form-control"></textarea>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="form-group">
+                            <label for="" class="form-control-label">Passport photo</label>
+                              <input type="file" class="form-control">
+                        </div>
+                    </div>
+                </div>
+                <input type="submit" value="Submit" class="btn btn-primary">
+                <input type="reset" value="Clear" class="btn btn-warning">
+            </form>
+        </div>
+    </main>
+    <footer>
+        <marquee behavior="alternate" direction="">
+            &copy; 2023 All Right Reserved <span>Developed By Omar, James, Sharon, Anthony, Faith and Cynthia</span>
+        </marquee>
+    </footer>
+    <script src="assets/js/custom.js"></script>
+    <script>
+        $(document).ready(()=>{})
+        $('.preloader').fadeOut('slow', function(){
+            $(this).remove()
+        }).delay(100)
+    </script>
+</body>
+</html>
