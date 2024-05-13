79f96a41a4b34e96938207dc6fc528fd1eb45e21
dillahmwangi <dillahmwangi@gmail.com>
2024-05-13 10:10:54 +0300

    New changes

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
     
     <header class="header">
         <div class="title-tab">
-            <a href="index.html" class="navbar-brand">
+            <a href="index.php" class="navbar-brand">
                 <div class="icon">
                     <img src="assets/images/arms.png" alt="">
                 </div>
@@ -46,7 +46,7 @@
             <span class="text-fade">navigation</span>
         </div>
         <div class="sidebar-menu">
-            <a href="index.html" class="link"><span class="feather icon-home"></span><span>Dashboard</span></a>
+            <a href="index.php" class="link"><span class="feather icon-home"></span><span>Dashboard</span></a>
             <div class="drop">
                 <span>
                     <span class="feather icon-clipboard"></span>
@@ -55,8 +55,8 @@
                 <span class="feather icon-chevron-right"></span>
             </div>
             <div class="drop-content">
-                <a href="add_task.html" class="link"><span class="feather icon-chevron-right"></span><span>New Task</span></a>
-                <a href="tasks.html" class="link"><span class="feather icon-chevron-right"></span><span>View Task</span></a>
+                <a href="add_task.php" class="link"><span class="feather icon-chevron-right"></span><span>New Task</span></a>
+                <a href="tasks.php" class="link"><span class="feather icon-chevron-right"></span><span>View Task</span></a>
             </div>
             <div class="drop">
                 <span>
@@ -66,10 +66,10 @@
                 <span class="feather icon-chevron-right"></span>
             </div>
             <div class="drop-content">
-                <a href="add_officer.html" class="link"><span class="feather icon-chevron-right"></span><span>New Officer</span></a>
-                <a href="officers.html" class="link"><span class="feather icon-chevron-right"></span><span>View Officer</span></a>
+                <a href="add_officer.php" class="link"><span class="feather icon-chevron-right"></span><span>New Officer</span></a>
+                <a href="officers.php" class="link"><span class="feather icon-chevron-right"></span><span>View Officer</span></a>
             </div>
-            <a href="#" class="link"><span class="feather icon-user"></span><span>Account Settings</span></a>
+            <a href="account.php" class="link"><span class="feather icon-user"></span><span>Account Settings</span></a>
         </div>
     </aside>
     <main class="content">
@@ -78,7 +78,7 @@
                 <h4>Dashboard</h4>
             </div>
             <div class="navigation">
-                <span><a href="index.html"><i class="feather icon-home"></i></a></span>
+                <span><a href="index.php"><i class="feather icon-home"></i></a></span>
                 <span>/</span>
                 <span class="text-fade">Dashboard</span>
             </div>
@@ -95,7 +95,7 @@
     </main>
     <footer>
         <marquee behavior="alternate" direction="">
-            &copy; 2023 All Right Reserved <span>Developed By Omar, James, Sharon, Anthony, Faith and Cynthia</span>
+            &copy; 2023 All Right Reserved <span>Developed By Omar, James, Sharon, Anthony, Faith, Charity and Deity</span>
         </marquee>
     </footer>
     <script src="assets/js/custom.js"></script>
