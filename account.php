6414d6844612318054fabff6a4c5a08bb80e6e09
karuku-design <annkaruku5832@gmail.com>
2024-05-18 07:08:53 +0300

    Eddited the account.php UI to the default one

--- account.php
+++ account.php
@@ -1,7 +1,12 @@
+<!-- view_officer.php -->
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Task Tracker</title>
     <link rel="stylesheet" href="assets/css/all.min.css">
@@ -16,7 +21,9 @@
     <script src="assets/js/jquery.dataTables.js"></script>
 </head>
 <body>
-<header class="header">
+    <div class="preloader"></div>
+
+    <header class="header">
         <div class="title-tab">
             <a href="index.php" class="navbar-brand">
                 <div class="icon">
@@ -28,12 +35,85 @@
                 <span class="feather icon-maximize scre"></span>
             </div>-->
         </div>
-           
+        <div class="profile-tab">
+            <div class="profile-photo">
+                <img src="assets/images/pic-1.png" alt="" class="image-responsive">
+            </div>
+            <div class="profile-description">
+                <span>omar mathias</span>
+                <a href="#"><span class="feather icon-power text-danger"></span></a>
+            </div>
         </div>
     </header>
-    <br>
-<a href="index.php" class="link"><span class="feather icon-user">HOME</a>
-<h1>ACCOUNT</h1>
 
+    <aside class="sidebar">
+        <div class="sidebar-header">
+            <span class="text-fade">navigation</span>
+        </div>
+        <div class="sidebar-menu">
+            <a href="index.php" class="link"><span class="feather icon-home"></span><span>Dashboard</span></a>
+            <div class="drop">
+                <span>
+                    <span class="feather icon-clipboard"></span>
+                    <span>Task</span>
+                </span>
+                <span class="feather icon-chevron-right"></span>
+            </div>
+            <div class="drop-content">
+                <a href="add_task.php" class="link"><span class="feather icon-chevron-right"></span><span>New Officer</span></a>
+                <a href="officers.php" class="link"><span class="feather icon-chevron-right"></span><span>View Officers</span></a>
+            </div>
+            <div class="drop">
+                <span>
+                    <span class="feather icon-users"></span>
+                    <span>Officers</span>
+                </span>
+                <span class="feather icon-chevron-right"></span>
+            </div>
+            <div class="drop-content">
+                <a href="add_officer.php" class="link"><span class="feather icon-chevron-right"></span><span>New Officer</span></a>
+                <a href="officers.php" class="link"><span class="feather icon-chevron-right"></span><span>View Officer</span></a>
+            </div>
+            <a href="account.php" class="link"><span class="feather icon-user"></span><span>Account Settings</span></a>
+        </div>
+    </aside>
+    <main class="content">
+        <div class="content-header">
+            <div class="title">
+                <h4>Task</h4>
+            </div>
+            <div class="navigation">
+                <span><a href="index.php"><i class="feather icon-home"></i></a></span>
+                <span>/</span>
+                <span class="text-fade">View officer</span>
+            </div>
+        </div>
+        <div class="content-body">
+            
+        </div>
+    </main>
+    <footer>
+        <marquee behavior="alternate" direction="">
+            &copy; 2023 All Right Reserved <span>Developed By Omar, James, Sharon, Anthony, Faith & Cynthia</span>
+            &copy; 2024 All Right Reserved <span>Developed By Ann, Deity, Charity, Delron, Brian, Keziah & Daniel </span>
+        </marquee>
+    </footer>
+    <script src="assets/js/custom.js"></script>
+    <script>
+        $(document).ready(()=>{
+            $('#table_id').DataTable({
+                scrollX: true,
+                scrollCollapse: true,
+                lengthMenu: [
+                    [10, 25, 50, 100, -1],
+                    [10, 25, 50, 100, 'All']
+                ]
+            });
+
+            $('.preloader').fadeOut('slow', function(){
+                $(this).remove()
+            }).delay(100)
+        })
+    </script>
 </body>
 </html>
