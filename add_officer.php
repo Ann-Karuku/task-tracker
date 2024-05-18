8fefd75d5c400828feeae9bcb394281066de3067
karuku-design <annkaruku5832@gmail.com>
2024-05-18 08:16:01 +0300

    Changed copy right text at add_officer.

--- add_officer.php
+++ add_officer.php
@@ -137,7 +137,7 @@
     <footer>
         <marquee behavior="alternate" direction="">
             &copy; 2023 All Right Reserved <span>Developed By Omar, James, Sharon, Anthony, Faith and Cynthia</span>
-            &copy; 2024 All Right Reserved <span>Developed By Ann, Deity, Charity, Delron, Brian, Keziah & Daniel </span>
+            &copy; 2024 <span>By Ann, Deity, Charity, Delron, Brian, Keziah & Daniel </span>
         </marquee>
     </footer>
     <script src="assets/js/custom.js"></script>
