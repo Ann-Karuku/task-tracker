e55ae53b1b20b4b906a54e48045c92594479117e
dillahmwangi <dillahmwangi@gmail.com>
2024-05-24 10:15:01 +0300

    Added delete functionality on officers.php

--- officers.php
+++ officers.php
@@ -155,7 +155,7 @@ $result = mysqli_query($conn, $sql);
 
                             ?>
                         <input type="hidden" name="Officer_Code" value="<?php  $record['Officer_Code'];?>">
-                            <a href="edit.php" class="btn btn-primary"><i class="feather icon-edit"></i></a>
+                        <a href="edit.php?Officer_Code=<?php echo $record['Officer_Code']?>" class="btn btn-primary"><i class="feather icon-edit"></i></a>
                      
                             <a href="?Officer_Code=<?php echo $record['Officer_Code']; ?>" class="btn btn-danger" onclick="confirmDelete(<?php echo $record['Officer_Code']; ?>)"><i class="feather icon-trash-2"></i></a>
                         </td>
@@ -174,12 +174,12 @@ $result = mysqli_query($conn, $sql);
     <script src="assets/js/custom.js"></script>
     <script>
 
-    // function confirmDelete(Officer_Code) {
-    //     var confirmDelete = confirm("Are you sure you want to delete this officer?");
-    //     if (confirmDelete) {
-    //         window.location.href = "?Officer_Code" + Officer_Code;
-    //     }
-    // }
+    function confirmDelete(Officer_Code) {
+        var confirmDelete = confirm("Are you sure you want to delete this officer?");
+        if (confirmDelete) {
+            window.location.href = "?Officer_Code" + Officer_Code;
+        }
+    }
 
         $(document).ready(()=>{
             $('#table_id').DataTable({
