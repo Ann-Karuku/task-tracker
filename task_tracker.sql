30c013e3b0f10e7423edefb4e59b82eeb702f175
karuku-design <annkaruku5832@gmail.com>
2024-05-30 10:07:36 +0300

    Added changes to the database file

--- task_tracker.sql
+++ task_tracker.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 15, 2024 at 03:42 PM
+-- Generation Time: May 30, 2024 at 09:06 AM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -43,7 +43,10 @@ CREATE TABLE `officers` (
 --
 
 INSERT INTO `officers` (`Officer_Code`, `Officer_Name`, `Officer_Designation`, `Department`, `Officer_Contact`, `Remarks`, `Profile_Pic`, `Password`) VALUES
-('001', 'Felix Kinyua', 'Admin', 'ICT', '254796253425', '', '', 'Felix123');
+('001', 'Felix Kinyua', 'Admin', 'ICT', '077777777777', '', '66545b7d791300.58080572.jpg', 'Felix123'),
+('003', 'Faith', 'Officer', 'ICT', '0712345678', 'excellent', '66545b7d791300.58080572.jpg', 'Faith123'),
+('004', 'Charity Mwangi', 'Officer', 'ICT', '0100000000', '', '6656efc7d59c09.38286005.jpg', '037f118af192c1db14d6a437df58dc39fdd2ea0ef0f739c764'),
+('007', 'Brian Rop', 'Officer', 'ICT', '0799999999', '', '6656eff323e168.02194605.jpg', 'd1905252451d2d5d664f7c4d382acd0258c7c076691edad039');
 
 -- --------------------------------------------------------
 
@@ -58,8 +61,18 @@ CREATE TABLE `tasks` (
   `Support_Request` varchar(256) NOT NULL,
   `Support_Given` varchar(256) NOT NULL,
   `Remarks` varchar(100) NOT NULL,
-  `Office_NO` varchar(50) NOT NULL
+  `Office_NO` varchar(50) NOT NULL,
+  `Department` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tasks`
+--
+
+INSERT INTO `tasks` (`Task_ID`, `Officer_Code`, `Date`, `Support_Request`, `Support_Given`, `Remarks`, `Office_NO`, `Department`) VALUES
+(5, '004', '2024-05-27', 'Network issues', 'Network troubleshooting', 'Failed', '1700', 'DIT'),
+(7, '004', '2024-05-27', 'Printer Paper Jam', 'Printer fixed', 'Successful', '1417', 'ICT'),
+(8, '004', '2024-05-27', 'Internet and network unavailability', 'Cable crimping and reconfiguration', 'Successful', '1411', 'HRM&D');
 
 --
 -- Indexes for dumped tables
@@ -86,7 +99,7 @@ ALTER TABLE `tasks`
 -- AUTO_INCREMENT for table `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `Task_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Task_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Constraints for dumped tables
@@ -96,7 +109,7 @@ ALTER TABLE `tasks`
 -- Constraints for table `tasks`
 --
 ALTER TABLE `tasks`
-  ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`Officer_Code`) REFERENCES `officers` (`Officer_Code`);
+  ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`Officer_Code`) REFERENCES `officers` (`Officer_Code`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
