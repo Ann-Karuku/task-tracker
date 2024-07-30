cd8180b676a75066322781704126db592a6a9adc
karuku-design <annkaruku5832@gmail.com>
2024-07-30 11:29:48 +0300

    Updated to capture email

--- forgot_pass.php
+++ forgot_pass.php
@@ -1,8 +1,3 @@
-<!-- view_officer.php -->
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,55 +5,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Task Tracker</title>
     <link rel="icon" href="favicon.ico" type="image/x-icon">
-
     <link rel="stylesheet" href="assets/css/all.min.css">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/fontawesome.min.css">
     <link rel="stylesheet" href="assets/icons/feather/css/feather.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/jquery.dataTables.css">
-
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
     <script src="assets/js/jquery.dataTables.js"></script>
 </head>
 <body>
     <div class="preloader"></div>
-
-
-        <div class="content-body">
-    <div class="row justify-content-center">
-                             <!-- display the error -->
-                            <?php if (isset($_GET['error'])) { ?>
-                                <p class="error"><?php echo $_GET['error']; ?></p>
-                            <?php } ?>
-
-                            <!-- Display success message -->
-                            <?php if (isset($_GET['success'])) { ?>
-                                        <p class="success"><?php echo $_GET['success']; ?></p>
-                            <?php } ?>
-        <form action="change_pass_val.php" method="post" >
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="form-group mb-3">
-                            <h3>Forgot Password</h3>
-                            <label for="" class="form-control-label">Email</label>
-                            <input type="password" name="Current_Password" class="form-control">
-                        </div>
-                        <div class="mb-3">
-                            <input type="submit" value="Send Password Reset Link" name="submit" class="btn btn-primary">
-                            <a href="account.php" class="btn btn-warning">Back</a>
+    <div class="content-body">
+        <div class="row justify-content-center">
+            <!-- display the error -->
+            <?php if (isset($_GET['error'])) { ?>
+                <p class="error"><?php echo $_GET['error']; ?></p>
+            <?php } ?>
+            <!-- Display success message -->
+            <?php if (isset($_GET['success'])) { ?>
+                <p class="success"><?php echo $_GET['success']; ?></p>
+            <?php } ?>
+            <form action="send_reset_link.php" method="post">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="form-group mb-3">
+                                <h3>Forgot Password</h3>
+                                <label for="email" class="form-control-label">Email</label>
+                                <input type="email" name="email" class="form-control" required>
+                            </div>
+                            <div class="mb-3">
+                                <input type="submit" value="Send Password Reset Link" name="submit" class="btn btn-primary">
+                                <a href="account.php" class="btn btn-warning">Back</a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </form>
+            </form>
+        </div>
     </div>
-</div>
-
-   
-    </main>
     <script src="assets/js/custom.js"></script>
     <script>
         $(document).ready(()=>{
@@ -70,20 +57,18 @@
                     [10, 25, 50, 100, 'All']
                 ]
             });
-
             $('.preloader').fadeOut('slow', function(){
                 $(this).remove()
             }).delay(100)
         })
     </script>
-     <script>
-    document.getElementById('logoutButton').onclick = function() {
-     var confirmLogout = confirm("Are you sure you want to log out?");
-        if (confirmLogout) {
-        // Redirect to logout page or call your PHP logout script
-        window.location.href = 'logout.php'; 
-        }
+    <script>
+        document.getElementById('logoutButton').onclick = function() {
+            var confirmLogout = confirm("Are you sure you want to log out?");
+            if (confirmLogout) {
+                window.location.href = 'logout.php'; 
+            }
         };
-  </script>
+    </script>
 </body>
 </html>
